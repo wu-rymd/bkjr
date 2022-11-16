8e128f4e0a1edb348895e52c3e092dd1d625e964
wu-rymd <wu.raymond@columbia.edu>
2022-11-15 22:40:22 -0500

    Add check if stock ID from HTTP request is valid before storing in database

---
+++ src/main/java/com/ase/restservice/service/FinanceService.java
@@ -0,0 +1,31 @@
+package com.ase.restservice.service;
+
+import java.io.IOException;
+import org.springframework.stereotype.Service;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+/**
+ * Service for Finance operations (involving Yahoo! Finance API)
+ */
+@Service
+public class FinanceService implements FinanceServiceI {
+    /**
+     * Checks whether a stock ID is valid.
+     *
+     * @param stockId Stock ID
+     * @return boolean true or false
+     */
+    public boolean isStockIdValid(String stockId) {
+        try {
+            Stock apiStock = YahooFinance.get(stockId);
+            if (apiStock == null) {
+                return false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+}
