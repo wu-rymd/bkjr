d01a214d4e037df9e23f127b7ad1babfde41a5e3
wu-rymd <wu.raymond@columbia.edu>
2022-11-16 17:38:23 -0500

    Connect to finance API to get real-time data, create stock with real-time price

--- src/main/java/com/ase/restservice/service/interface/FinanceServiceI.java
+++ src/main/java/com/ase/restservice/service/interface/FinanceServiceI.java
@@ -1,5 +1,9 @@
 package com.ase.restservice.service;
 
+import com.ase.restservice.exception.ResourceNotFoundException;
+import com.ase.restservice.model.Stock;
+import java.io.IOException;
+
 /**
  * Interface for Finance service (involving Yahoo! Finance API)
  */
@@ -11,4 +15,23 @@ public interface FinanceServiceI {
      * @return boolean true or false
      */
     boolean isStockIdValid(String stockId);
+
+    /**
+     * Gets the price for a given stock ID.
+     *
+     * @param stockId Stock ID
+     * @return Float real-time value of the stock
+     * @throws ResourceNotFoundException if the stock ID is invalid
+     */
+    Float getStockPrice(String stockId) throws ResourceNotFoundException, IOException;
+
+    /**
+     * Creates a Stock object in the database with the current real-time price given a stock ID.
+     *
+     * @param stockId Stock ID
+     * @return Instantiated Stock object with current real-time price
+     * @throws ResourceNotFoundException if the stock ID is invalid
+     * @throws IOException when there is a connection error
+     */
+    Stock createStockFromId(String stockId) throws ResourceNotFoundException, IOException;
 }
