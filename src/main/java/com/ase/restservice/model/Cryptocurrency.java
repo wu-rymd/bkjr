ee1eb5a91375a9d6147970e69f3bfa132928be99
Unal Yigit Ozulku <uyozulku@gmail.com>
2022-11-25 15:28:47 -0500

    add crypto and nft models

---
+++ src/main/java/com/ase/restservice/model/Cryptocurrency.java
@@ -0,0 +1,84 @@
+package com.ase.restservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Represents Cryptocurrency table.
+ */
+@Entity
+@Table(name = "cryptocurrency")
+public class Cryptocurrency {
+
+    private String cryptocurrencyId;
+    private Float price;
+
+    /**
+     * Default constructor for Cryptocurrency.
+     */
+    public Cryptocurrency() {
+
+    }
+
+    /**
+     * Constructor for Cryptocurrency.
+     * 
+     * @param cryptocurrencyId unique Id for cryptocurrency
+     * @param price            price of cryptocurrency
+     */
+    public Cryptocurrency(final String cryptocurrencyId, final Float price) {
+        this.cryptocurrencyId = cryptocurrencyId;
+        this.price = price;
+    }
+
+    /**
+     * Getter for cryptocurrencyId.
+     * 
+     * @return cryptocurrencyId
+     */
+    @Id
+    public String getCryptocurrencyId() {
+        return cryptocurrencyId;
+    }
+
+    /**
+     * Setter for cryptocurrencyId.
+     * 
+     * @param cryptocurrencyId cryptocurrencyId
+     */
+    public void setCryptocurrencyId(final String cryptocurrencyId) {
+        this.cryptocurrencyId = cryptocurrencyId;
+    }
+
+    /**
+     * Getter for price.
+     * 
+     * @return price
+     */
+    @Column(name = "price", nullable = false)
+    public Float getPrice() {
+        return price;
+    }
+
+    /**
+     * Setter for price.
+     * 
+     * @param price price
+     */
+    public void setPrice(final Float price) {
+        this.price = price;
+    }
+
+    /**
+     * Override toString method.
+     * 
+     * @return String representation of Cryptocurrency
+     */
+
+    @Override
+    public String toString() {
+        return "Cryptocurrency [cryptocurrencyId=" + cryptocurrencyId + ", price=" + price + "]";
+    }
+}
---
+++ src/main/java/com/ase/restservice/model/NFT.java
@@ -0,0 +1,82 @@
+package com.ase.restservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Represents NFT table.
+ */
+@Entity
+@Table(name = "nft")
+public class NFT {
+
+    private String nftId;
+    private Float price;
+
+    /**
+     * Default constructor for NFT.
+     */
+    public NFT() {
+
+    }
+
+    /**
+     * Constructor for NFT.
+     * 
+     * @param nftId unique Id for nft
+     * @param price price of nft
+     */
+    public NFT(final String nftId, final Float price) {
+        this.nftId = nftId;
+        this.price = price;
+    }
+
+    /**
+     * Getter for nftId.
+     * 
+     * @return nftId
+     */
+    @Id
+    public String getNftId() {
+        return nftId;
+    }
+
+    /**
+     * Setter for nftId.
+     * 
+     * @param nftId nftId
+     */
+    public void setNftId(final String nftId) {
+        this.nftId = nftId;
+    }
+
+    /**
+     * Getter for price.
+     * 
+     * @return price
+     */
+    @Column(name = "price", nullable = false)
+    public Float getPrice() {
+        return price;
+    }
+
+    /**
+     * Setter for price.
+     * 
+     * @param price price
+     */
+    public void setPrice(final Float price) {
+        this.price = price;
+    }
+
+    /**
+     * Override toString method.
+     */
+
+    @Override
+    public String toString() {
+        return "NFT [nftId=" + nftId + ", price=" + price + "]";
+    }
+}
