e96978281cad77b5de5c62e1a300fa8f04324be3
jlam <joeylamborn@gmail.com>
2022-12-01 13:02:21 -0500

    integration tests for crypto, finance, stock, and nft, update integration-test profile so that auth is not active when integration-tests are run

---
+++ src/integration-test/java/com/ase/restservice/ITStock.java
@@ -0,0 +1,83 @@
+package com.ase.restservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.ase.restservice.model.Stock;
+import com.ase.restservice.repository.AccountRepository;
+import com.ase.restservice.repository.AssetRepository;
+import com.ase.restservice.repository.StockRepository;
+import com.ase.restservice.repository.TransactionRepository;
+import com.ase.restservice.service.AccountService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ITStock {
+
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+  @Autowired
+  private AssetRepository assetRepository;
+  @Autowired
+  private AccountService accountService;
+  @Autowired
+  private AccountRepository accountRepository;
+  @Autowired
+  private StockRepository stockRepository;
+  @Autowired
+  private TransactionRepository transactionRepository;
+
+  @DisplayName("Integration test for updateStockPrice endpoint verifying that the "
+      + "controller, service and repo layers function correctly together")
+  @Test
+  public void ITUpdateStockPriceSuccessWorksThroughAllLayers() throws Exception {
+    Stock testStock = new Stock("c482ba45-7", 100F);
+
+    if (stockRepository.existsById(testStock.getStockId())) {
+      stockRepository.deleteById(testStock.getStockId());
+    }
+    stockRepository.save(testStock);
+
+    mockMvc.perform(put("/stocks/{stockId}/{stockPrice}",
+        testStock.getStockId(), 50))
+        .andExpect(status().isOk());
+
+    assertTrue(stockRepository.existsById(testStock.getStockId()));
+    Optional<Stock> resStock = stockRepository.findById(testStock.getStockId());
+    assertEquals(50, resStock.get().getPrice());
+  }
+
+  @DisplayName("Integration test for updateStockPrice endpoint when stock DNE verifying that "
+      + "controller, service and repo layers function correctly together")
+  @Test
+  public void ITUpdateStockPriceDNEWorksThroughAllLayers() throws Exception {
+    Stock testStock = new Stock("c482ba45-7", 100F);
+
+    if (stockRepository.existsById(testStock.getStockId())) {
+      stockRepository.deleteById(testStock.getStockId());
+    }
+    // Now stock does not exist in database
+    mockMvc.perform(put("/stocks/{stockId}/{stockPrice}",
+            testStock.getStockId(), 50))
+        .andExpect(status().is4xxClientError());
+  }
+}
--- src/integration-test/java/com/ase/restservice/ITTransactions.java
+++ src/integration-test/java/com/ase/restservice/ITTransactions.java
@@ -15,10 +15,11 @@ import com.ase.restservice.repository.AssetRepository;
 import com.ase.restservice.repository.StockRepository;
 import com.ase.restservice.repository.TransactionRepository;
 import com.ase.restservice.service.AccountService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,16 +28,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ITbuyStock {
+public class ITTransactions {
 
   @Autowired
   private MockMvc mockMvc;
-
   @Autowired
   private ObjectMapper objectMapper;
   @Autowired
@@ -50,26 +49,57 @@ public class ITbuyStock {
   @Autowired
   private TransactionRepository transactionRepository;
 
-  @DisplayName("Integration test between controller, service, and repo layers to verify sending a"
-      + " buy transaction to the service results in a new asset being created, transaction being"
-      + " stored and the transaction status being updated. ")
-  @Test
-  void buyStockWorksThroughAllLayers() throws Exception {
-    Account testAccount = new Account(
+  private Account testAccount;
+  private AssetId expectedAssetID;
+  private Stock testStock;
+  @BeforeEach
+  public void setup() {
+
+    testAccount = new Account(
         "43ad9cea-f",
         100F,
         100F,
         "binance");
 
+    testStock = new Stock("ce07363b-f", 13.44F);
+    expectedAssetID = new AssetId(testAccount.getAccountId(),
+        "stock", testStock.getStockId());
+
+    // GET AND DELETE EXISTING TRANSACTIONS FOR TEST ACCOUNT
+    Optional<List<Transaction>> existingTransactions =
+        transactionRepository.findByAccountId(testAccount.getAccountId());
+    if (existingTransactions.isPresent()) {
+      for (Transaction transaction: existingTransactions.get()
+      ) { transactionRepository.deleteById(transaction.getUuid());
+      }
+    }
+    List<Asset> assets = assetRepository.findAllAssetsByAccountId(testAccount.getAccountId());
+
+    // GET AND DELETE PREVIOUS ASSETS FOR TEST ACCOUNT
+    if (assets.size() > 0) {
+      for (Asset asset : assets
+      ) {assetRepository.deleteById(asset.getAssetId()); }
+    }
     if (accountRepository.existsById(testAccount.getAccountId())) {
       accountRepository.deleteById(testAccount.getAccountId());
     }
-    accountRepository.save(testAccount);
 
-    Stock testStock = new Stock("ce07363b-f", 13.44F);
+    testStock = new Stock("ce07363b-f", 13.44F);
     if (stockRepository.existsById(testStock.getStockId())) {
       stockRepository.deleteById(testStock.getStockId());
     }
+
+  }
+  @AfterEach
+  public void cleanup() {
+  }
+  @DisplayName("Integration test between controller, service, and repo layers to verify sending a"
+      + " buy transaction to the service results in a new asset being created, transaction being"
+      + " stored and the transaction status being updated. ")
+  @Test
+  public void ITbuyStockWorksThroughAllLayers() throws Exception {
+
+    accountRepository.save(testAccount);
     stockRepository.save(testStock);
 
     Transaction buyTransaction = new Transaction(
@@ -93,39 +123,22 @@ public class ITbuyStock {
     assertTrue(transactions.isPresent());
     assertEquals("COMPLETED", transactions.get().get(0).getTransactionStatus());
 
-    AssetId expectedAssetId = new AssetId(
-        testAccount.getAccountId(),"stock", testStock.getStockId());
 
     // Get new asset from database
-    Optional<Asset> asset = assetRepository.findById(expectedAssetId);
+    Optional<Asset> asset = assetRepository.findById(expectedAssetID);
     assertTrue(asset.isPresent());
 
-    // Cleanup, delete the new asset, transaction
-    assetRepository.deleteById(expectedAssetId);
-    transactionRepository.deleteById(transactions.get().get(0).getUuid());
   }
 
   @Test
   public void ITSellStock() throws Exception {
-    Account testAccount = new Account(
-        "43ad9cea-f",
-        100F,
-        100F,
-        "binance");
 
-    if (accountRepository.existsById(testAccount.getAccountId())) {
-      accountRepository.deleteById(testAccount.getAccountId());
-    }
     accountRepository.save(testAccount);
-
-    Stock testStock = new Stock("ce07363b-f", 13.44F);
-    if (stockRepository.existsById(testStock.getStockId())) {
-      stockRepository.deleteById(testStock.getStockId());
-    }
     stockRepository.save(testStock);
 
     Asset userAsset = new Asset(testAccount.getAccountId(),
         "stock", testStock.getStockId(), 1000F);
+
     assetRepository.save(userAsset);
 
     Transaction sellTransaction = new Transaction(
@@ -146,6 +159,7 @@ public class ITbuyStock {
         transactionRepository.findByAccountId(testAccount.getAccountId());
 
     assertTrue(transactions.isPresent());
+    assertEquals(1, transactions.get().size());
     assertEquals("COMPLETED", transactions.get().get(0).getTransactionStatus());
 
     AssetId expectedAssetId = new AssetId(
@@ -156,11 +170,6 @@ public class ITbuyStock {
     assertTrue(asset.isPresent());
     Float expectedQuantity = userAsset.getQuantity() - sellTransaction.getQuantity();
     assertEquals(expectedQuantity, asset.get().getQuantity());
-
-    // Cleanup, delete the new asset
-    assetRepository.deleteById(expectedAssetId);
-    transactionRepository.deleteById(transactions.get().get(0).getUuid());
-
   }
 
 
